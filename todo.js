a158b7c2b7c04aca47aa6f681560a34cd32e825c
R-Nerio <r.nerio.477839@umindanao.edu.ph>
2024-08-21 15:37:29 +0800

    second

--- todo.js
+++ todo.js
@@ -27,6 +27,7 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
+console.log("second commit");
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
